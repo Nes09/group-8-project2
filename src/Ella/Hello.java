24f950fc0cf398eca1d2e3f453b3f430f6a931de
emuratalieva <emuratalieva@gmail.com>
2021-08-06 10:37:34 -0400

    1st commit from TC1111

---
+++ src/Ella/Hello.java
@@ -0,0 +1,12 @@
+package Ella;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello to everyone!");
+
+    }
+}
